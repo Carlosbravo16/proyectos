4549b8d863650bd3be535f9452c3e557708007b5
Carlosbravo16 <110642356+Carlosbravo16@users.noreply.github.com>
2023-11-28 21:25:46 -0500

    se le agrega un metodo de funcion

--- proyectotarjeta/proyectoTarjeta.js
+++ proyectotarjeta/proyectoTarjeta.js
@@ -2,25 +2,29 @@ const inputTrajeta = document.getElementById("inputTarjeta");
 const inputFecha = document.getElementById("inputFecha");
 const inputCVV = document.getElementById("inputCVV");
 
+//delimita la cantidad de datosque puede ingresar el usuario y los separa por "-" y "/"
 const mascaraNumero = "####-####-####-####";
 const mascaraFecha ="##/##";
 const mascaraCVV = "###";
 
+//arrays para guardar lo que ingresa el usuario
 let TarjetaNumero = [];
 let FechaNumero = [];
 let cvvNumero =[];
 
-inputTrajeta.addEventListener("keydown", (e) => {
-    if (e.key === "Tab") {
+inputTrajeta.addEventListener("keydown", (e) => {  //"e" me guarda la tecla que se imprima
+    if (e.key === "Tab" && TarjetaNumero.length < 16 ){
+        alert("Faltan numeros por digitar")
         return;
     }
-    e.preventDefault();
+    e.preventDefault(); //evita que se refresque la pantalla y que se digite caracteres no permitidos
     ingresoDatos(mascaraNumero, e.key, TarjetaNumero);
     inputTrajeta.value = TarjetaNumero.join("");
 });
 
 inputFecha.addEventListener("keydown", (e) => {
-    if (e.key === "Tab") {
+    if (e.key === "Tab" && TarjetaNumero.length < 4) {
+        alert("Faltan numeros por digitar")
         return;
     }
     e.preventDefault();
@@ -29,7 +33,8 @@ inputFecha.addEventListener("keydown", (e) => {
 });
 
 inputCVV.addEventListener("keydown", (e) => {
-    if (e.key === "Tab") {
+    if (e.key === "Tab" && TarjetaNumero.length < 3) {
+        alert("Faltan numeros por digitar")
         return;
     }
     e.preventDefault();
@@ -41,11 +46,11 @@ function ingresoDatos(mascara, key, arreglo){
     let numeros = ["0", "1", "2", "3", " 4", "5", "6", "7", "8", "9"];
     if (key === "Backspace" && arreglo.length > 0) {
         arreglo.pop();
-        return;
+        return; // "pop" borra lo ultimo que se escribio de arreglo 
     }
     if (numeros.includes(key) && arreglo.length + 1 <= mascara.length){
      if (mascara[arreglo.length] === "-" || mascara[arreglo.length] === "/") {
-        arreglo.push(mascara[arreglo.length], key);
+        arreglo.push(mascara[arreglo.length], key); //"push" el arreglo lo envia "
      } else {
         arreglo.push(key);
      }
